eebc6690edec9d26a295224cc9d973b97dc590b7
AnkitSinghD10S <147189008+AnkitSinghD10S@users.noreply.github.com>
2024-10-28 22:53:48 +0530

    updates

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <header className='max-w-screen-2xl mx-auto px-4 py-6 border-slate-700'>
+        <nav className='flex justify-between items-center'>
+            <div>
+                logo
+            </div>
+
+            <div>
+                nav items
+            </div>
+        </nav>
+    </header>
+  )
+}
+
+export default Navbar
