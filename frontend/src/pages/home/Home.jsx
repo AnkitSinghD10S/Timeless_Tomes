8f72061abde20ed4fb00a678368e23dd8bf2e065
AnkitSinghD10S <147189008+AnkitSinghD10S@users.noreply.github.com>
2024-11-02 22:23:17 +0530

    top seller

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Banner from './Banner'
 import TopSeller from './TopSellers'
+import Recommened from './Recommened'
 
 const Home = () => {
   return (
     <>
     <Banner/>
     <TopSeller/>
+    <Recommened/>
     </>
   )
 }
