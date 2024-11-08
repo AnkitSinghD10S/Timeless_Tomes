f73dc917a126b0bf887e1bbfa3e53e133af4e327
AnkitSinghD10S <147189008+AnkitSinghD10S@users.noreply.github.com>
2024-11-08 23:20:02 +0530

    cart functionality

--- frontend/src/routers/router.jsx
+++ frontend/src/routers/router.jsx
@@ -3,6 +3,7 @@ import App  from "../App"
 import Home from "../pages/home/Home";
 import Login from "../components/Login";
 import Register from "../components/Register";
+import CartPage from "../pages/books/CartPage";
 
 const router = createBrowserRouter([
   {
@@ -22,16 +23,16 @@ const router = createBrowserRouter([
             element: <h1>About</h1>
         },
         {
-            path:"/cart",
-            element: <h1>Cart</h1>
-        },
-        {
             path:"/login",
             element:<Login/>
         },
         {
             path:"/register",
             element:<Register/>
+        },
+        {
+            path:"/cart",
+            element:<CartPage/>
         }
     ]
   },
